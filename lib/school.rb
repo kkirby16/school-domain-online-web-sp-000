fd87fedc42e9c35037933032827621cf0cac3af8
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-20 14:41:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,10 +12,8 @@ class School
   def add_student(name, grade) 
     if @roster[grade] == nil 
       @roster[grade] = [name] 
-      @roster 
     elsif @roster[grade] != nil 
     @roster[grade] << name
-    @roster
     end
   end
   
